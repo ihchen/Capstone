5b37abf7d573476ef726aae4f9cd6f9549579c76
Wesley Stedman <wstedman@pugetsound.edu>
2016-02-03 19:23:08 -0800

    added makeMusicSnippet.js

---
+++ scripts/makeMusicSnippet.js
@@ -0,0 +1,23 @@
+function makeScale(scaleType, key) {
+	// open data.csv read-only
+
+	// get scale instructions
+
+	// transpose
+
+	// return assembled scale
+}
+
+function makeChord(chordType, key) {
+	// open data.csv read-only
+
+	// get chord instructions
+
+	// transpose
+
+	// return assembled scale
+}
+
+function makeInterval() {
+	// I don't know what to do here
+}
