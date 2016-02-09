bf1c11539439718f61b362f0df3e342ef962228c
Wesley Stedman <wstedman@pugetsound.edu>
2016-02-08 17:59:22 -0800

    minor update to makeMusicSnippet.js

--- scripts/makeMusicSnippet.js
+++ scripts/makeMusicSnippet.js
@@ -1,41 +1,43 @@
-function makeScale(scaleType, key) {
-	// open data.csv read-only
-	var data = new XMLHttpRequest();
-	data.open("GET", "data.csv");
-	data.send();
-	// get scale instructions
-	var scale;
-	var lines = data.response.split("\n");
-	for (var i = lines.length - 1; i >= 0; i--) {
-		var fields = lines[i].split(",");
-		if (fields[0]=="scale" && fields[1]==scaleType) {
-			scale = fields[2];
+class MakeMusicSnippet {
+	function makeScale(scaleType, key) {
+		// open data.csv read-only
+		var data = new XMLHttpRequest();
+		data.open("GET", "data.csv");
+		data.send();
+		// get scale instructions
+		var scale;
+		var lines = data.response.split("\n");
+		for (var i = lines.length - 1; i >= 0; i--) {
+			var fields = lines[i].split(","); // fields = a line of the csv
+			if (fields[0]=="scale" && fields[1]==scaleType) {
+				scale = fields[2].split(" "); // scale = the notes in an array
+			};
 		};
-	};
-	// transpose
+		// transpose
 
-	// return assembled scale
-}
+		// return assembled scale
+	}
 
-function makeChord(chordType, key) {
-	// open data.csv read-only
-	var data = new XMLHttpRequest();
-	data.open("GET", "data.csv");
-	data.send();
-	// get chord instructions
-	var chord;
-	var lines = data.response.split("\n");
-	for (var i = lines.length - 1; i >= 0; i--) {
-		var fields = lines[i].split(",");
-		if (fields[0]=="chord" && fields[1]==chordType) {
-			chord = fields[2];
+	function makeChord(chordType, key) {
+		// open data.csv read-only
+		var data = new XMLHttpRequest();
+		data.open("GET", "data.csv");
+		data.send();
+		// get chord instructions
+		var chord;
+		var lines = data.response.split("\n");
+		for (var i = lines.length - 1; i >= 0; i--) {
+			var fields = lines[i].split(",");
+			if (fields[0]=="chord" && fields[1]==chordType) {
+				chord = fields[2];
+			};
 		};
-	};
-	// transpose
+		// transpose
 
-	// return assembled scale
-}
+		// return assembled scale
+	}
 
-function makeInterval() {
-	// I don't know what to do here
+	function makeInterval() {
+		// I don't know what to do here
+	}
 }
