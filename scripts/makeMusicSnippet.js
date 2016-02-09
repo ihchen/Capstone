8016b22ac3bc4de1a78e383240150831d00a90c3
Wesley Stedman <wstedman@pugetsound.edu>
2016-02-08 17:35:09 -0800

    updated makeMusicSnippet.js to read and parse data.csv

--- scripts/makeMusicSnippet.js
+++ scripts/makeMusicSnippet.js
@@ -1,8 +1,17 @@
 function makeScale(scaleType, key) {
 	// open data.csv read-only
-
+	var data = new XMLHttpRequest();
+	data.open("GET", "data.csv");
+	data.send();
 	// get scale instructions
-
+	var scale;
+	var lines = data.response.split("\n");
+	for (var i = lines.length - 1; i >= 0; i--) {
+		var fields = lines[i].split(",");
+		if (fields[0]=="scale" && fields[1]==scaleType) {
+			scale = fields[2];
+		};
+	};
 	// transpose
 
 	// return assembled scale
@@ -10,9 +19,18 @@ function makeScale(scaleType, key) {
 
 function makeChord(chordType, key) {
 	// open data.csv read-only
-
+	var data = new XMLHttpRequest();
+	data.open("GET", "data.csv");
+	data.send();
 	// get chord instructions
-
+	var chord;
+	var lines = data.response.split("\n");
+	for (var i = lines.length - 1; i >= 0; i--) {
+		var fields = lines[i].split(",");
+		if (fields[0]=="chord" && fields[1]==chordType) {
+			chord = fields[2];
+		};
+	};
 	// transpose
 
 	// return assembled scale
