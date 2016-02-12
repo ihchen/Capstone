af5738c8dee6b170c47742ec74ddf2f66009bf41
Wesley Stedman <wstedman@pugetsound.edu>
2016-02-12 15:49:21 -0800

    started html/php page to allow users to play anything in the data file

---
+++ selectSample.php
@@ -0,0 +1,21 @@
+<?php
+	// load scripts/data.csv
+?>
+
+<form>
+	<select name="class">
+		<?php
+			// first column options
+		?>
+	</select>
+	<select name="type">
+		<?php
+			// second column options
+		?>
+	</select>
+	<select name="key">
+		<?php
+			// key options
+		?>
+	</select>
+</form>
