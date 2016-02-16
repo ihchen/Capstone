d57aa0993aa9daf8fae3479758fc6d1e55a4a910
Kaylene <kbarber@pugetsound.edu>
2016-02-16 03:55:34 -0800

    Set octave function and relevant tests. Fixed interval issue.

--- scripts/Note.js
+++ scripts/Note.js
@@ -1,16 +1,25 @@
-// This file contains a variety of methods that perform musical
-// algorithms on a collection of notes.
+// This file contains a variety of methods that perform useful
+// algorithms on a collection of musical notes.
 
 // An array of musical notes in order of the circle of fifths.
+// DO NOT CHANGE THIS ORDER!!!
 const NOTES = ["Fbb", "Cbb", "Gbb", "Dbb", "Abb", "Ebb", "Bbb", "Fb",
   "Cb", "Gb", "Db", "Ab", "Eb", "Bb", "F", "C", "G", "D", "A", "E",
   "B", "Fx", "Cx", "Gx", "Dx", "Ax", "Ex", "Bx", "Fxx", "Cxx", "Gxx",
   "Dxx", "Axx", "Exx", "Bxx"];
 
+// Returns the position of a note in the circle of fifths.
+function ordinal(note) {
+  for (var i = 0; i < NOTES.length; i++) {
+    // Test Strings for equality.
+    if (note == NOTES[i]) return i;
+  }
+}
+
 // Takes an array of notes and shifts them over by the specified
 // amount in the circle of fifths.
 // Take care with providing the amount to shift; you don't want to
-// shift far enough to create an unused spelling. Because the shift
+// shift far enough to create an unused spelling. Since the shift
 // argument must be in fifths, it is a range that can be easily
 // determined.
 function transpose(notes, shift) {
@@ -18,10 +27,12 @@ function transpose(notes, shift) {
 
   // For each note in the array arg, find it in our circle of
   // fifths array.
-  for (i = 0; i < notes.length; i++) {
-    for (j = 0; j < NOTES.length; j++) {
+  for (var i = 0; i < notes.length; i++) {
+    for (var j = 0; j < NOTES.length; j++) {
       // Test Strings for equality.
       if (NOTES[j] == notes[i]) {
+        // TODO: Simplify this!!!
+        // var ordinal = ordinal(notes[i]);
         // We have found the note in the circle of fifths!
         // Offset it and add it to the new array.
         transposedNotes[transposedNotes.length] = NOTES[j + shift];
@@ -51,39 +62,32 @@ function setInversion(chord, inversion) {
   return chord;
 }
 
-// Reference array for the distance up to get to an interval, in order
-// of the circle of fifths. Each number corresponds to the number of
-// half steps between the following intervals, respectively:
-// d5, m2, m6, m3, m7, p4, p8, p5, M2, M6, M3, M7, a4
+// Reference array for the ascending distance to an interval.
+// Each number corresponds to the number of half steps between
+// the following intervals, respectively:
+// unison, p5, M2, M6, M3, M7, tritone, m2, m3, m7, p4
 // DO NOT CHANGE THE ORDER!
-const INTERVALS = [6, 1, 8, 3, 10, 5, 12, 7, 2, 9, 4, 11, 6];
-// The space containing 12 really represents unison, but for our purposes,
-// we will assume that calculating that interval of unison is trivial.
-// If we get two of the same note in a row, (which we won't in the
-// actual program) we will treat it as the octave above.
+const INTERVALS = [0, 7, 2, 9, 4, 11, 6, 1, 8, 3, 10, 5];
 
-// The difference between the placement of two notes in the circle
-// of fifths needs to be offset by this number to reach the correct
-// index in the INTERVAL array.
-const SHIFT = 6;
+// The number of half steps in an octave.
+const OCTAVE = 12;
 
 // Calculate the interval between two notes.
 // Returns the interval as a number of half steps.
 function calcInterval(note1, note2) {
-  // Find note1 in our circle of fifths array.
-  var i;
-  for (i = 0; i < NOTES.length; i++) {
-    if (note1 == NOTES[i]) break;
-  }
-  // Find note2 in our circle of fifths array.
-  var j;
-  for (j = 0; j < NOTES.length; j++) {
-    if (note2 == NOTES[j]) break;
+  // Subtract Note1's ordinal from Note2's ordinal
+  var ordinalDiff = ordinal(note2) - ordinal(note1);
+  //document.write("ordinalDiff = " + ordinalDiff +"<br>");
+
+  // Normalize
+  if (!(ordinalDiff < OCTAVE && ordinalDiff > -OCTAVE)) {
+    ordinalDiff %= OCTAVE;
   }
 
-  // Subtract Note1's index from Note2's index and add SHIFT to
-  // obtain the correct index for INTERVAL.
-  return INTERVALS[j - i + SHIFT];
+  // Bring negative numbers up into the positives!
+  if (ordinalDiff < 0) ordinalDiff += OCTAVE;
+
+  return INTERVALS[ordinalDiff];
 }
 
 // Calculates the span of a collection of notes. Returns an
@@ -91,47 +95,99 @@ function calcInterval(note1, note2) {
 // note and highest note in the given note collection.
 function calcSpan(notes) {
   var span = 0;
-  for (i = 0; i < notes.length - 1; i++) {
+  for (var i = 0; i < notes.length - 1; i++) {
     span += calcInterval(notes[i], notes[i + 1]);
   }
   return span;
 }
 
+// The number of notes available for playback ranging from
+// C3 to B5.
+const NUM_NOTES = 35;
+
+// The lowest note available for playback is C3, and its
+// ordinal in the circle of fifths is 15.
+const C = 15;
+
 // Sets an octave appropriate for the given span of notes.
 // Does so by appending a number to the end of each String.
+// DO NOT USE ON A SCALE OR INTERVAL THAT HAS BEEN REVERSED.
+// PLEASE REVERSE AFTER SETTING THE OCTAVE!!!
+// Inverted chords are O.K.
 function setOctave(notes) {
-  var span = calcSpan(notes);
-  // TODO: WRITE THIS!!!
+
+  // The distance from C (the lowest note available) to the
+  // first note of the note collection.
+  var span = calcInterval(NOTES[C], notes[0]);
+
+  // Incorporate the total span of the note collection.
+  span += calcSpan(notes);
+
+  var numPlaces = 1;
+  while (span <= NUM_NOTES/numPlaces && numPlaces <=5) {
+    numPlaces++;
+  }
+  // This note collection will fit in (numPlaces - 1) places.
+  // Decrement to reflect that.
+  numPlaces--;
+
+  if (numPlaces == 0) {
+    // This note collection will not fit.
+    console.log("Warning: This note collection either has too "
+      + "large a span, or the span is large with too high of"
+      + " a starting note. Please check your inputs!")
+    return null;
+  }
+
+  // Let's pick one of those places at random.
+  var octave = genRandomNum(numPlaces);
+  var octavizedNotes = [];
+  for (var i = 0; i < notes.length; i++) {
+    if (i > 0) {
+      if (passedC(notes[i - 1], notes[i])) {
+        // If we have just passed C between here and the previous
+        // note, then we need to increment the octave.
+        octave++;
+      }
+    }
+    // Concat octave onto the note name!
+    octavizedNotes.push(notes[i] + octave);
+  }
+
+  return octavizedNotes;
 }
 
-// The number of half steps in an octave.
-const OCTAVE = 12;
+// Checks to see if a C was passed between two notes.
+// This includes Cbb, C, Cx, and Cxx
+function passedC(note1, note2) {
 
-// Basically a "compareTo()" method to test for enharmonic equivalence
-function isEnharmonic(note1, note2) {
-  // Find the first note.
-  var i = 0;
-  for (i; i < NOTES.length; i++) {
-    if(NOTES[i] == note1) {
-      // We found it!
-      break;
+  // We need a special case if the first note IS C.
+  if (note1.charAt(0) == 'C' || calcInterval(note1, NOTES[C]) == 0) {
+    // We literally just changed the octave, so don't do it again!
+    return false;
+  }
+  // Likewise, if the second note is some kind of C
+  else if (note2.charAt(0) == 'C') {
+    // We have reached C! Time to increment the octave!
+    return true;
+  }
+  // Otherwise, check the distance to the next note.
+  else if (calcInterval(note1, NOTES[C]) <= calcInterval(note1, note2)) {
+    if (note1 == "Dbb") {
+      document.write("WHOA");
     }
+    return true;
+  } else {
+    return false;
   }
+}
 
-  // Check octave above.
-  var j = i + OCTAVE;
-  while (j < notes.length) {
-    if (NOTES[j] == NOTES[i]) return true;
-    above += OCTAVE;
-  }
-
-  // Check octave below.
-  j = i - OCTAVE;
-  while (j > 0) {
-    if (NOTES[j] == NOTES[i]) return true;
-    above -= OCTAVE;
-  }
-
-  // If we've made it here, we couldn't find equivalence.
-  return false;
+// Generate a random number for the octave in which a note
+// collection may begin.
+const lowestOctave = 3;
+function genRandomNum(numPlaces) {
+  // 3 is the lowest octave in which a note collection may begin,
+  // and numPlaces provides a range. This method will return a
+  // random number from lowestOctave to (numPlaces - 1) inclusive.
+  return Math.floor((Math.random() * (numPlaces)) + lowestOctave);
 }
