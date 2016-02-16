0e9c0df541709796f304deb28d336e8a4c98e225
Wesley Stedman <wstedman@pugetsound.edu>
2016-02-15 18:52:20 -0800

    small additions to php files

--- selectSample.php
+++ selectSample.php
@@ -1,21 +1,32 @@
 <?php
 	// load scripts/data.csv
+	$datacsv = fopen("scripts/data.csv", r) or die("Unable to open data.csv!");
+	// parse
+	$types = array();
+	$keys = array("C","D","E");
+
+	while (!feof($datacsv)) {
+		$data_entry = str_getcsv(fgets($datacsv));
+		array_push($types, $data_entry[1]+" "+$data_entry[0]);
+	}
+
+	fclose($datacsv);
 ?>
 
 <form>
-	<select name="class">
-		<?php
-			// first column options
-		?>
-	</select>
 	<select name="type">
 		<?php
-			// second column options
+			for ($i=0; $i < count($types); $i++) { 
+				echo "<option value='$types[i]'>$types[i]</option>\n";
+			}
 		?>
+	</select>
 	</select>
 	<select name="key">
 		<?php
-			// key options
+			for ($i=0; $i < count($keys); $i++) { 
+				echo "<option value='$keys[i]'>$keys[i]</option>\n";
+			}
 		?>
 	</select>
 </form>
---
+++ setupQuiz.php
@@ -0,0 +1,11 @@
+<form>
+	<fieldset>
+		<legend>Scales</legend>
+	</fieldset>
+	<fieldset>
+		<legend>Chords</legend>
+	</fieldset>
+	<fieldset>
+		<legend>Intervals</legend>
+	</fieldset>
+</form>
