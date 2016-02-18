a05825db721927579497488afc4ddfd6700435b0
Wesley Stedman <wstedman@pugetsound.edu>
2016-02-17 17:21:52 -0800

    added script tags to selectSample.js

--- selectSample.php
+++ selectSample.php
@@ -1,3 +1,8 @@
+<script type="text/javascript" src="scripts/Note.js"></script>
+<script type="text/javascript" src="scripts/makeMusicSnippet.js"></script>
+<script type="text/javascript" src="scripts/noteToFileNum.js"></script>
+<script type="text/javascript" src="scripts/MusicSnippet.js"></script>
+
 <?php
 	// load scripts/data.csv
 	$datacsv = fopen("scripts/data.csv", r) or die("Unable to open data.csv!");
