37724e483e0bb43c4de3917cbb6ff47c455cbeff
Wesley Stedman <wstedman@pugetsound.edu>
2016-02-24 17:56:31 -0800

    new file parsing script!

---
+++ scripts/ParseCSV.js
@@ -0,0 +1,24 @@
+/**
+ * This script loads the content of data.csv into a global variable, 'data'.
+ * 'data' is a 2D array where the first level is a line of the csv, and the
+ * second level is a column of the csv.
+ */
+
+var data = [];
+
+// get csv from the server
+var file = new XMLHttpRequest();
+file.open("GET", "scripts/data.csv", false);
+file.send();
+
+// break response into lines
+var lines = file.response.split("\n");
+
+// process each line
+for (var i = 0; i < lines.length; i++) {
+	if (lines[i] != "") { // if not an empty line
+		var tmp = lines[i].split(","); // split line
+		tmp[2] = tmp[2].split(" "); // change list of notes from string into array of strings
+		data.push(tmp); // push the line into the global
+	}
+}
