879a4f99151dc1b388baac99936444c87fba6f41
Wesley Stedman <wstedman@pugetsound.edu>
2016-03-02 17:12:31 -0800

    takeQuiz might be error free now...

--- takeQuiz.php
+++ takeQuiz.php
@@ -15,26 +15,24 @@
 
 	// Make an integer array to hold selected types of things to be tested on
 	var chosen = [];
+</script>
 
-	<?php
-	// convert post vars into javascript
-	foreach ($_POST as $key => $value) {
-		if ($key == "scaleUP" ||
-			$key == "scaleDOWN" ||
-			$key == "root" ||
-			$key == "first" ||
-			$key == "second" ||
-			$key == "third" ||
-			$key == "intervalUP" ||
-			$key == "intervalDOWN") {
-			echo "config.$key = true;";
-		}
-		else {
-			echo "chosen.push($key);";
-		}
+<?php
+// convert post vars into javascript
+foreach ($_POST as $key => $value) {
+	if ($value == "opt") {
+		echo "<script type='text/javascript'>config.$key = true;</script>";
 	}
-	?>
+	else if ($value == "num") {
+		echo "<script type='text/javascript'>chosen.push($key);</script>";
+	}
+	else {
+		echo "<script type='text/javascript'>console.log(\"POST Format Error: $key, $value\");</script>";
+	}
+}
+?>
 
+<script type="text/javascript">
 	console.log(config);
 	console.log(chosen);
 </script>
