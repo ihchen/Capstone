0d8009636387422af4cac27329483aa5fcda6696
Imanuel Chen <ihchen@pugetsound.edu>
2016-03-23 17:44:14 -0700

    sdjlfkaj

--- scripts/MusicSnippet.js
+++ scripts/MusicSnippet.js
@@ -158,13 +158,13 @@ function MusicSnippet(type, quality, notes, category) {
 		if(type == SCALE) {
 			if(i < numNotes -1) {
 				timeouts.push(setTimeout(function() {
-					tempSounds[i].fadeOut(0.3, (1/bps)*400);
-				}, (1/bps)*900));
+					tempSounds[i].fadeOut(0.3, (1/bps)*800);
+				}, (1/bps)*400));
 			}
 			else {
 				timeouts.push(setTimeout(function() {
-					tempSounds[i].fadeOut(0.0, (1/bps)*400);
-				}, (1/bps)*900));
+					tempSounds[i].fadeOut(0.0, (1/bps)*1000);
+				}, (1/bps)*400));
 			}
 		}		
 	}
