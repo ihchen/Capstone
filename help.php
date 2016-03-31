c4dd042bdcd4f7103cae9d93afac32138f5910e0
Kaylene <kbarber@pugetsound.edu>
2016-03-31 16:37:28 -0700

    adding help page

---
+++ help.php
@@ -0,0 +1,25 @@
+<?php
+$thisPage = 'HELP!!!';
+?>
+
+<?php require_once('phpincludes/header.php'); ?>
+
+<body>
+<br></br>
+
+<div id ="instructions">
+Welcome to the help page! You are probably here because something isn't working. Keep in mind that this web application is still in development. Thank you for your patience!
+<br></br>
+If you would like to give feedback or report an error, please email Kaylene Barber at kbarber@pugetsound.edu, describe the error in detail, specify which web browser you were using when the error occured, and if possible explain what you did to produce the error.
+</div>
+<br></br>
+<span class="qa">Q. </span>The sound isn't working!
+<br></br>
+<span class="qa">A. </span> First, refresh your web browser. If that doesn't work, clear your cache, quit your web browser entirely, and try again. If none of that works, try using Firefox or Google Chrome if you are not already.
+</body>
+
+<script type="text/javascript">
+
+</script>
+
+<?php require_once('phpincludes/footer.php'); ?>
