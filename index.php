ee981761fd1322d4c58abfa4e4faf66597a5e886
Imanuel Chen <ihchen@pugetsound.edu>
2016-04-16 18:55:40 -0700

    scalability

--- index.php
+++ index.php
@@ -1,177 +1,213 @@
-
-<?php
-$thisPage = 'Design Quiz';
+<?php 
+	$thisPage = 'Design Quiz';
+	require_once('phpincludes/header.php'); 
 ?>
 
-<?php require_once('phpincludes/header.php'); ?>
+<script src="scripts/ParseCSV.js"></script>
+<link href="style/style2.css" type="text/css" rel="stylesheet">
 
-	<title> <?php echo "$thisPage" ?> </title>
+<script>
+	/** 
+	 * Checks the boxes of the given html class. If maincheck is given, all boxes are checked
+	 * to how maincheck is checked. If not, then all boxes are just checked.
+	 *
+	 * @method checkBoxes
+	 * @param {String} checkbox_class Class of checkboxes to be checked/unchecked
+	 * @param {String} maincheck (Optional) Checkbox ID to determine whether or not to check/uncheck the given checkboxes
+	 */
+	function checkBoxes(checkbox_class, maincheck) {
+		var mainbox;
+		var subboxes = document.getElementsByClassName(checkbox_class);
 
-	<!-- Bootstrap -->
-	<!-- <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"> -->
-	<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script> -->
-	<!-- <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script> -->
-
-	<!-- CSV Parsing script -->
-	<script src="scripts/ParseCSV.js"></script>
-
-	<link href="style/style2.css" type="text/css" rel="stylesheet">
-
-	<script>
-		/* Activate Collapsible list */
-		// $('.collapse').collapse();
-
-		/* Checks the boxes of the given class */
-		function checkBoxes(checkbox_class, mainclass) {
-			var mainbox;
-			var subboxes = document.getElementsByClassName(checkbox_class);
-			if(mainclass != undefined) {
-				mainbox = document.getElementById(mainclass);
-			}
-			for(var i = 0; i < subboxes.length; i++) {
-				if(mainbox == undefined || mainbox.checked) {
-					subboxes[i].checked = true;
-				}
-				else {
-					subboxes[i].checked = false;
-				}
-			}
-			checkCheckedChords();
-			checkCheckedScales();
+		//If maincheck is given, grab the checkbox element
+		if(maincheck != undefined) {
+			mainbox = document.getElementById(maincheck);
 		}
-
-		function checkTest2() {
-			var test2 = document.getElementById('test2box');
-			if(test2.checked)
-				document.getElementById('test1box').checked = true;
-			else
-				document.getElementById('test1box').checked = false;
-			checkBoxes('Test2', 'test2box');
-			checkBoxes('Test1', 'test1box');
-		}
-
-		function checkFinal() {
-			var final = document.getElementById('finalbox');
-			if(final.checked)
-				document.getElementById('test2box').checked = true;
-			else
-				document.getElementById('test2box').checked = false;
-			checkTest2();
-			checkBoxes('Final', 'finalbox');
-		}
-
-		function uncheckBoxes(type) {
-			var boxes;
-			if(type == 'chord') {
-				boxes = document.getElementsByClassName("type2chord");
-				document.getElementById("chordselect").style.display = "block";
-				document.getElementById("chorddeselect").style.display = "none";
+		//Loop through all checkboxes given and check accordingly
+		for(var i = 0; i < subboxes.length; i++) {
+			//Check by default or if mainbox is also checked
+			if(mainbox == undefined || mainbox.checked) {
+				subboxes[i].checked = true;
 			}
-			else if(type == 'scale') {
-				boxes = document.getElementsByClassName("type2scale");
-				document.getElementById("scaleselect").style.display = "block";
-				document.getElementById("scaledeselect").style.display = "none";
-			}
+			//Uncheck if mainbox is unchecked
 			else {
-				boxes = document.getElementsByTagName("input");
-				document.getElementById("chordselect").style.display = "block";
-				document.getElementById("chorddeselect").style.display = "none";
-				document.getElementById("scaleselect").style.display = "block";
-				document.getElementById("scaledeselect").style.display = "none";
-			}
-			for(var i = 0; i < boxes.length; i++) {
-				if(boxes[i].type == "checkbox") {
-					boxes[i].checked = false;
-				}
+				subboxes[i].checked = false;
 			}
 		}
+		//When something is checked, check all chords and scales and update select all buttons
+		checkCheckedChords();
+		checkCheckedScales();
+	}
 
-		function checkCheckedChords() {
-			var hasChecked = false;
-			var boxes = document.getElementsByClassName("type2chord");
-			for(var i = 0; i < boxes.length; i++) {
-				if(boxes[i].checked == true) {
-					hasChecked = true;
-					document.getElementById("chordselect").style.display = "none";
-					document.getElementById("chorddeselect").style.display = "block";
-				}
-			}
-			if(!hasChecked) {
-				document.getElementById("chordselect").style.display = "block";
-				document.getElementById("chorddeselect").style.display = "none";
+	/**
+	 * Check/uncheck all boxes in Test 1
+	 *
+	 * @method checkTest1
+	 */
+	function checkTest1() {
+		checkBoxes('Test1','test1box');
+	}
+
+	/**
+	 * Check/uncheck all boxes in Test 2 and Test 1
+	 *
+	 * @method checkTest2
+	 */
+	function checkTest2() {
+		var test2 = document.getElementById('test2box');
+		if(test2.checked)
+			document.getElementById('test1box').checked = true;
+		else
+			document.getElementById('test1box').checked = false;
+		checkBoxes('Test2', 'test2box');
+		checkTest1();
+	}
+
+	/**
+	 * Check/uncheck all boxes in Final, Test 2, and Test 1
+	 *
+	 * @method checkFinal
+	 */
+	function checkFinal() {
+		var final = document.getElementById('finalbox');
+		if(final.checked)
+			document.getElementById('test2box').checked = true;
+		else
+			document.getElementById('test2box').checked = false;
+		checkBoxes('Final', 'finalbox');
+		checkTest2();
+	}
+
+	/**
+	 * Uncheck the given boxes. If type not given, uncheck all boxes. Update select/deselect all
+	 * buttons
+	 *
+	 * @method uncheckBoxes
+	 * @param {String} type (Optional) If type given, only uncheck boxes of given type
+	 */
+	function uncheckBoxes(type) {
+		var boxes;
+		if(type == 'chord') {
+			boxes = document.getElementsByClassName("type2chord");
+			document.getElementById("chordselect").style.display = "block";
+			document.getElementById("chorddeselect").style.display = "none";
+		}
+		else if(type == 'scale') {
+			boxes = document.getElementsByClassName("type2scale");
+			document.getElementById("scaleselect").style.display = "block";
+			document.getElementById("scaledeselect").style.display = "none";
+		}
+		else {
+			boxes = document.getElementsByTagName("input");
+			document.getElementById("chordselect").style.display = "block";
+			document.getElementById("chorddeselect").style.display = "none";
+			document.getElementById("scaleselect").style.display = "block";
+			document.getElementById("scaledeselect").style.display = "none";
+		}
+		for(var i = 0; i < boxes.length; i++) {
+			if(boxes[i].type == "checkbox") {
+				boxes[i].checked = false;
 			}
 		}
+	}
 
-		function checkCheckedScales() {
-			var hasChecked = false;
-			var boxes = document.getElementsByClassName("type2scale");
-			for(var i = 0; i < boxes.length; i++) {
-				if(boxes[i].checked == true) {
-					hasChecked = true;
-					document.getElementById("scaleselect").style.display = "none";
-					document.getElementById("scaledeselect").style.display = "block";
-				}
-			}
-			if(!hasChecked) {
-				document.getElementById("scaleselect").style.display = "block";
-				document.getElementById("scaledeselect").style.display = "none";
+	/**
+	 * Checks to see if there are any checked chords. If so, use deselect all button. Otherwise
+	 * use select all button
+	 *
+	 * @method checkCheckedChords
+	 */
+	function checkCheckedChords() {
+		var hasChecked = false;
+		var boxes = document.getElementsByClassName("type2chord");
+		for(var i = 0; i < boxes.length; i++) {
+			if(boxes[i].checked == true) {
+				hasChecked = true;
+				document.getElementById("chordselect").style.display = "none";
+				document.getElementById("chorddeselect").style.display = "block";
+				break;
 			}
 		}
-	</script>
-<!-- </head>
+		if(!hasChecked) {
+			document.getElementById("chordselect").style.display = "block";
+			document.getElementById("chorddeselect").style.display = "none";
+		}
+	}
 
+	/**
+	 * Checks to see if there are any checked scales. If so, use deselect all button, Otherwise, 
+	 * use select all button.
+	 */
+	function checkCheckedScales() {
+		var hasChecked = false;
+		var boxes = document.getElementsByClassName("type2scale");
+		for(var i = 0; i < boxes.length; i++) {
+			if(boxes[i].checked == true) {
+				hasChecked = true;
+				document.getElementById("scaleselect").style.display = "none";
+				document.getElementById("scaledeselect").style.display = "block";
+				break;
+			}
+		}
+		if(!hasChecked) {
+			document.getElementById("scaleselect").style.display = "block";
+			document.getElementById("scaledeselect").style.display = "none";
+		}
+	}
+</script>
 
-<center>
-	<h1> UPS Ear Training </h1>
-</center> -->
 <style>
 * {
 	box-sizing: border-box;
 }
 .rows {
-	display: flex;
+	display: flex;			/* Line bottoms of columns up */
 }
-.colspace {
+.colspace {					/* Blank space (input &nbsp in divs with this class) */
 	width: 16.66%;
 	float: left;
 }
-.maincolumn {
+#specialspace {
+	width: 0%;
+	float: left;
+}
+.maincolumn {				/* Column that contains information */
 	width: 33.33%;
 	float: left;
 	padding: 5px;
 	text-align: left;
 	border: 1px solid #008B8B;
 }
-.typehead {
+.typehead {					/* Type title (chord/scale) */
 	font-size: 1.2em;
 	font-weight: bold;
 	border-bottom: 2px solid #008B8B;
 	position:relative;
-}
+}							/* List of chords/scales */
 .type2list {
 	-webkit-column-count: 2;
 	-moz-column-count: 2;
 	column-count: 2;
 }
-.category {
+.category {					/* Class for each category */
 	-webkit-column-break-inside: avoid; /* Chrome, Safari */
     page-break-inside: avoid;           /* Theoretically FF 20+ */
     break-inside: avoid-column;         /* IE 11 */
     display:table;                      /* Actually FF 20+ */
 }
-.categorytitle {
+.categorytitle {			/* Title of category */
 	font-size: 1.1em;
 }
-.type2 {
+.type2 {					/* Every element */
 	font-weight: 100;
 }
-.testnames {
+.testnames {				/* Test 1, Test 2, Final */
 	font-size: 1.3em;
 	display: inline-block;
 	padding-left: 5px;
 	padding-right: 20px;
 }
-.selectbtn {
+.selectbtn {				/* Select/Deselect Buttons */
 	text-align: center;
 	white-space: nowrap;
 	background: #008B8B;
@@ -187,67 +223,142 @@ $thisPage = 'Design Quiz';
 .selectbtn:hover {
   background: #00CED1;
 }
+
+/* Resizing and Scalability */
+@media screen and (max-width: 1420px) {
+	.colspace {
+		width: 8.33%;
+	}
+	.maincolumn {
+		width: 41.66%;
+	}
+}
+
+@media screen and (max-width: 1135px) {
+	.colspace {
+		width: 1%;
+	}
+	.maincolumn {
+		width: 49%;
+	}
+}
+
+@media screen and (max-width: 954px) {
+	.colspace {
+		width: 16.66%;
+	}
+	.maincolumn {
+		width: 33.33%;
+	}
+	.type2list {
+		-webkit-column-count: 1;
+		-moz-column-count: 1;
+		column-count: 1;
+	}
+}
+
+@media screen and (max-width: 710px) {
+	.colspace {
+		width: 8.33%;
+	}
+	.maincolumn {
+		width: 41.66%;
+	}
+}
+
+@media screen and (max-width: 565px) {
+	.colspace {
+		width: 1%;
+	}
+	.maincolumn {
+		width: 49%;
+	}
+}
+
+@media screen and (max-width: 480px) {
+	.colspace {
+		width: 5%;
+		height: 200%;
+	}
+	.maincolumn {
+		width: 90%;
+	}
+	.rows {
+		display: block;
+	}
+	#specialspace {
+		clear: left;
+		width: 5%;
+	}
+}
 </style>
+
 <body id = "content">
-
 	<div id = "instructions">
-		Click on the words "Chords" or "Scales" to expand the dropdown list. Select items you would like to practice identifying. Click "Start Training" to begin your practice quiz.
+		Select items you would like to practice identifying. Click "Start Training" to begin your practice quiz.
 	</div>
 
-<form name="selection" action="takeQuiz.php" onsubmit="return validateForm()" method="post">
-	<input type="checkbox" id="test1box" onclick="checkBoxes('Test1','test1box')"><div class="testnames"> Test 1 </div>
-	<input type="checkbox" id="test2box" onclick="checkTest2()"><div class="testnames"> Test 2 </div>
-	<input type="checkbox" id="finalbox" onclick="checkFinal()"><div class="testnames"> Final </div>
-	<br/>
-	<button type="button" class="button" onclick="uncheckBoxes()">Deselect All</button>
-	<br/>
+	<form name="selection" action="takeQuiz.php" onsubmit="return validateForm()" method="post">
+		<!-- Test checkboxes -->
+		<input type="checkbox" id="test1box" onclick="checkTest1()"><div class="testnames"> Test 1 </div>
+		<input type="checkbox" id="test2box" onclick="checkTest2()"><div class="testnames"> Test 2 </div>
+		<input type="checkbox" id="finalbox" onclick="checkFinal()"><div class="testnames"> Final </div>
+		<br/>
+		<button type="button" class="button" onclick="uncheckBoxes()">Deselect All</button>
+		<br/>
 
-	<div class="rows">
-		<div class="colspace">&nbsp</div>
-		<div class="maincolumn">
-			<div class="typehead">
-				Chords
-				<button id="chordselect" type="button" class="selectbtn" onclick="checkBoxes('type2chord')">Select All</button>
-				<button id="chorddeselect" type="button" class="selectbtn" onclick="uncheckBoxes('type2chord')">Deselect All</button>
-			</div>
-			<div class="type2list" id="chord">
-				<!-- <div class="category"> -->
-					<!-- <div class="categorytitle">[category]</div> -->
-					<!-- <div class="type2" id="[category]"> -->
-						<!-- Generated scales go here -->
+		<div class="rows">
+			<div class="colspace">&nbsp</div>
+
+			<!-- Chords column -->
+			<div class="maincolumn">
+				<div class="typehead">
+					Chords
+					<!-- Buttons -->
+					<button id="chordselect" type="button" class="selectbtn" onclick="checkBoxes('type2chord')">Select All</button>
+					<button id="chorddeselect" type="button" class="selectbtn" onclick="uncheckBoxes('type2chord')">Deselect All</button>
+				</div>
+				<div class="type2list" id="chord">
+					<!-- <div class="category"> -->
+						<!-- <div class="categorytitle">[category]</div> -->
+						<!-- <div class="type2" id="[category]"> -->
+							<!-- Generated scales go here -->
+						<!-- </div> -->
 					<!-- </div> -->
-				<!-- </div> -->
-				<!-- ... -->
+					<!-- ... -->
+				</div>
 			</div>
-		</div>
-		<div class="maincolumn">
-			<div class="typehead">
-				Scales
-				<button id="scaleselect" type="button" class="selectbtn" onclick="checkBoxes('type2scale')">Select All</button>
-				<button id="scaledeselect" type="button" class="selectbtn" onclick="uncheckBoxes('type2scale')">Deselect All</button>
-			</div>
-			<div class="type2list" id="scale">
-				<!-- <div class="category"> -->
-					<!-- <div class="categorytitle">[category]</div> -->
-					<!-- <div class="type2" id="[category]"> -->
-						<!-- Generated chords go here -->
+
+			<div class="colspace" id="specialspace">&nbsp</div>
+
+			<!-- Scales column -->
+			<div class="maincolumn">
+				<div class="typehead">
+					Scales
+					<button id="scaleselect" type="button" class="selectbtn" onclick="checkBoxes('type2scale')">Select All</button>
+					<button id="scaledeselect" type="button" class="selectbtn" onclick="uncheckBoxes('type2scale')">Deselect All</button>
+				</div>
+				<div class="type2list" id="scale">
+					<!-- <div class="category"> -->
+						<!-- <div class="categorytitle">[category]</div> -->
+						<!-- <div class="type2" id="[category]"> -->
+							<!-- Generated chords go here -->
+						<!-- </div> -->
 					<!-- </div> -->
-				<!-- </div> -->
-				<!-- ... -->
+					<!-- ... -->
+				</div>
 			</div>
+
+			<div class="colspace">&nbsp</div>
 		</div>
-		<div class="colspace">&nbsp</div>
-	</div>
-	<br/>
-	<input class = "button" type="submit" value="Start Training">
+		<br/>
+		<input class = "button" type="submit" value="Start Training">
+		<br/><br/><br/>
+	</form>
 </body>
 
-
-</form>
-
 <script>
-	checkCheckedChords();
-	checkCheckedScales();
+	// Find all categories and save a tuple containing (category, occurence, type)
 	var categories = [];
 
 	for (var i = 0; i < data.length; i++) {
@@ -266,6 +377,13 @@ $thisPage = 'Design Quiz';
 		}
 	}
 
+	/**
+	 * http://www.stoimen.com/blog/2010/07/09/friday-algorithms-javascript-bubble-sort/
+	 * Didn't feel like coding bubblesort...
+	 *
+	 * @method bubbleSort
+	 * @param {2D Array} a Sorts by the second element in each subarray.
+	 */
 	function bubbleSort(a) {
 	    var swapped;
 	    do {
@@ -281,8 +399,10 @@ $thisPage = 'Design Quiz';
 	    } while (swapped);
 	}
 
+	//Sort categories so that we can display shortest ones first (looks nicer on UI)
 	bubbleSort(categories);
 
+	//For all categories, create divs. Id to put elements in is '[category][type]'.
 	for (var i = 0; i < categories.length; i++) {
 		var div = document.getElementById(categories[i][2]);
 		div.innerHTML = div.innerHTML +
@@ -291,6 +411,7 @@ $thisPage = 'Design Quiz';
 			"<div class='type2' id='"+categories[i][0]+categories[i][2]+"'></div></div>";
 	}
 
+	//Put every element in csv file into the corresponding divs
 	for (var i = 0; i < data.length; i++) {
 		var div = document.getElementById(data[i][4]+data[i][0]);
 		if(data[i][0] == 'chord') {
@@ -314,7 +435,11 @@ $thisPage = 'Design Quiz';
 		}
 	}
 
-	// if the form doesn't have at least one box checked, alert and stop submission
+	/**
+	 * If the form doesn't have at least one box checked, alert and stop submission
+	 *
+	 * @method validateForm
+	 */
 	function validateForm() {
 		var form_fields = document.forms["selection"]; // get the form
 		for (var i = 0; form_fields[i.toString()] != undefined; i++) { // loop through everything in the form
@@ -328,6 +453,11 @@ $thisPage = 'Design Quiz';
 		return false; // don't submit the form
 	}
 
+	/**
+	 * Make cookies to remember what the user selected
+	 *
+	 * @method makeCookie
+	 */
 	function makeCookie() {
 		var cookie_string = "selected=";
 		var form_fields = document.forms["selection"];
@@ -341,6 +471,10 @@ $thisPage = 'Design Quiz';
 		cookie_string += "; expires=" + exp.toUTCString();
 		document.cookie = cookie_string;
 	}
+
+	//Check checked chords and scales in case cookies were saved or user hit refresh
+	checkCheckedChords();
+	checkCheckedScales();
 </script>
 
 <?php require_once('phpincludes/footer.php'); ?>
