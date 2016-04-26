eaa15cc093410dd5d88090f8f57f346412ae9a2a
Kaylene <kbarber@pugetsound.edu>
2016-04-25 19:15:13 -0700

    SMI UI

--- phpincludes/footer.php
+++ phpincludes/footer.php
@@ -1,6 +1,6 @@
 <br/><br/><br/>
 <div id="footer">
-  <span class="footertext">&copy; <a href="">University of Puget Sound 2016</a></span>
+  <span class="footertext">&copy; <a href="https://www.pugetsound.edu" style="color:black; text-decoration:none;">University of Puget Sound</a> 2016</span>
   <a href = "help.php" class ="help" title = "???">HELP!!!</a>
 <!-- </div> -->
 </html>
